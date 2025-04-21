dd558da41708bf0e89153aa0c189d85d9248b051
araujo.arthur <1arthurluiz@gmail.com>
2025-04-21 12:21:38 -0300

    venv update

--- POC2-keras/venv_py39_new/Scripts/activate.bat
+++ POC2-keras/venv_py39_new/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\bergc\pxg-bot\POC2-keras\venv_py39_new
+set VIRTUAL_ENV=C:\Users\arthu\codes\pokemon-identifier-poc\POC2-keras\venv_py39_new
 
 if not defined PROMPT set PROMPT=$P$G
 
