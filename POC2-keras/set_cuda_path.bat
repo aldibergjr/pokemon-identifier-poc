2041ce1de8c1af1edd51ac2632ec5cd8ddc25ea9
araujo.arthur <1arthurluiz@gmail.com>
2025-04-21 12:22:05 -0300

    deleted files

--- POC2-keras/set_cuda_path.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-set PATH=%PATH%;C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.8\bin;C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.8\libnvvp
-echo CUDA paths added to PATH 
